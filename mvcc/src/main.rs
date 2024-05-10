249465e33fc8716d54417a8d9763086c60429857
roseduan <duanmengmeng@hashdata.cn>
2024-05-10 22:43:57 +0800

    update mvcc

--- mvcc/src/main.rs
+++ mvcc/src/main.rs
@@ -72,10 +72,11 @@ pub struct Transaction {
 impl Transaction {
     // 开启事务
     pub fn begin(kv: Arc<Mutex<KVEngine>>) -> Self {
-        // 获取全局事务号
+        // 获取全局事务版本号
         let version = acquire_next_version();
 
         let mut active_txn = ACTIVE_TXN.lock().unwrap();
+        // 这个 map 的 key 就是当前所有活跃的事务
         let active_xid = active_txn.keys().cloned().collect();
 
         // 添加到当前活跃事务 id 列表中
@@ -101,12 +102,13 @@ impl Transaction {
 
     fn write(&self, key: &[u8], value: Option<Vec<u8>>) {
         // 判断当前写入的 key 是否和其他的事务冲突
+        // key 是按照 key-version 排序的，所以只需要判断最近的一个 key 即可
         let mut kvengine = self.kv.lock().unwrap();
         for (enc_key, _) in kvengine.iter().rev() {
             let key_version = decode_key(enc_key);
             if key_version.raw_key.eq(key) {
                 if !self.is_visible(key_version.version) {
-                    panic!("serialization error");
+                    panic!("serialization error, try again.");
                 }
                 break;
             }
@@ -127,6 +129,7 @@ impl Transaction {
         kvengine.insert(enc_key.encode(), value);
     }
 
+    // 读取数据，从最后一条数据进行遍历，找到第一条可见的数据
     pub fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
         let kvengine = self.kv.lock().unwrap();
         for (k, v) in kvengine.iter().rev() {
@@ -138,18 +141,27 @@ impl Transaction {
         None
     }
 
+    // 打印出所有可见的数据
     fn print_all(&self) {
+        let mut records = BTreeMap::new();
         let kvengine = self.kv.lock().unwrap();
-        for (k, v) in kvengine.iter().rev() {
+        for (k, v) in kvengine.iter() {
             let key_version = decode_key(k);
             if self.is_visible(key_version.version) {
-                println!(
-                    "key = {:?}, value = {:?}",
-                    String::from_utf8(key_version.raw_key.to_vec()),
-                    String::from_utf8(v.clone().unwrap().to_vec())
+                records.insert(key_version.raw_key.to_vec(), v.clone());
+            }
+        }
+
+        for (k, v) in records.iter() {
+            if let Some(value) = v {
+                print!(
+                    "{}={} ",
+                    String::from_utf8_lossy(k),
+                    String::from_utf8_lossy(value)
                 );
             }
         }
+        println!("");
     }
 
     // 提交事务
@@ -193,22 +205,58 @@ impl Transaction {
 fn main() {
     let eng = KVEngine::new();
     let mvcc = MVCC::new(eng);
+    // 先新增几条数据
+    let tx0 = mvcc.begin_transaction();
+    tx0.set(b"a", b"a1".to_vec());
+    tx0.set(b"b", b"b1".to_vec());
+    tx0.set(b"c", b"c1".to_vec());
+    tx0.set(b"d", b"d1".to_vec());
+    tx0.set(b"e", b"e1".to_vec());
+    tx0.commit();
+
+    // 开启一个事务
     let tx1 = mvcc.begin_transaction();
+    // 将 a 改为 a2，e 改为 e2
+    tx1.set(b"a", b"a2".to_vec());
+    tx1.set(b"e", b"e2".to_vec());
+    // Time
+    //  1  a2              e2
+    //  0  a1  b1  c1  d1  e1
+    //     a   b   c   d   e   Keys
 
-    tx1.set(b"a", b"val1".to_vec());
-    tx1.set(b"a", b"val11".to_vec());
+    // t1 虽然未提交，但是能看到自己的修改了
+    tx1.print_all(); // a=a2 b=b1 c=c1 d=d1 e=e2
 
-    tx1.set(b"b", b"val2".to_vec());
-    tx1.set(b"c", b"val3".to_vec());
-
-    // tx1.commit();
-    // tx1.rollback();
-
+    // 开启一个新的事务
     let tx2 = mvcc.begin_transaction();
-    tx2.print_all();
-    tx2.set(b"d", b"val22".to_vec());
-    tx2.set(b"d", b"val23".to_vec());
+    // 删除 b
+    tx2.delete(b"b");
+    // Time
+    //  2      X
+    //  1  a2              e2
+    //  0  a1  b1  c1  d1  e1
+    //     a   b   c   d   e   Keys
 
+    // 此时 T1 没提交，所以 T2 看到的是
+    tx2.print_all(); // a=a1 c=c1 d=d1 e=e1
+                     // 提交 T1
     tx1.commit();
-    tx2.print_all();
+    // 此时 T2 仍然看不到 T1 的提交，因为 T2 开启的时候，T2 还没有提交（可重复读）
+    tx2.print_all(); // a=a1 c=c1 d=d1 e=e1
+
+    // 再开启一个新的事务
+    let tx3 = mvcc.begin_transaction();
+    // Time
+    //  3
+    //  2      X               uncommitted
+    //  1  a2              e2  committed
+    //  0  a1  b1  c1  d1  e1
+    //     a   b   c   d   e   Keys
+    // T3 能看到 T1 的提交，但是看不到 T2 的提交
+    tx3.print_all(); // a=a2 b=b1 c=c1 d=d1 e=e2
+
+    // T3 写新的数据
+    tx3.set(b"f", b"f1".to_vec());
+    // T2 写同样的数据，会冲突
+    tx2.set(b"f", b"f1".to_vec());
 }
